2725ad1c93f521b928a9ad7d70b0419296d44511
vobear <vobear@126.com>
2021-01-12 13:48:24 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: 文件描述
+ * @Author: daxiong
+ * @Date: 2021-01-12 13:23:32
+ * @LastEditors: daxiong
+ * @LastEditTime: 2021-01-12 13:26:52
+ * @FilePath: e:\npm-dev\react-native-show-action-sheet\src\index.js
+ */
+
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class extends Component {
+    render() {
+        return (
+            <View><Text>测试</Text></View>
+        )
+    }
+}
