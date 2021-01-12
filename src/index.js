d5f06fe5dfd72981ac9271c3126500714484325c
vobear <vobear@126.com>
2021-01-12 14:31:15 +0800

    update class name

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@
  * @Author: daxiong
  * @Date: 2021-01-12 13:23:32
  * @LastEditors: daxiong
- * @LastEditTime: 2021-01-12 14:09:10
+ * @LastEditTime: 2021-01-12 14:30:25
  * @FilePath: e:\npm-dev\react-native-show-action-sheet\src\index.js
  */
 
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-export default class showActionSheet extends Component {
+export default class ShowActionSheet extends Component {
     render() {
         return (
             <View><Text>测试</Text></View>
