19c1ce2165b41cd48846d8dd35b8d4f41cf8aea1
vobear <vobear@126.com>
2021-01-25 15:29:31 +0800

    update main file

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 文件描述
+ * @Author: daxiong
+ * @Date: 2021-01-25 15:13:21
+ * @LastEditors: daxiong
+ * @LastEditTime: 2021-01-25 15:13:21
+ * @FilePath: e:\npm-dev\react-native-show-action-sheet\index.js
+ */
+import ShowActionSheet from "./src/ShowActionSheet";
+
+export {
+    ShowActionSheet
+}
--- src/ShowActionSheet.js
+++ src/ShowActionSheet.js
@@ -3,13 +3,13 @@
  * @Author: daxiong
  * @Date: 2021-01-12 13:23:32
  * @LastEditors: daxiong
- * @LastEditTime: 2021-01-12 14:30:25
- * @FilePath: e:\npm-dev\react-native-show-action-sheet\src\index.js
+ * @LastEditTime: 2021-01-25 15:16:01
+ * @FilePath: e:\npm-dev\react-native-show-action-sheet\src\ShowActionSheet.js
  */
 
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { View, Text } from "react-native";
-export default class ShowActionSheet extends Component {
+export default class ShowActionSheet extends PureComponent {
     render() {
         return (
             <View><Text>测试</Text></View>
